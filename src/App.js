58dda67543814bbf08f8e3d30f82eb29064cc501
Vadim Fattakhov <vadfthv@gmail.com>
2022-06-21 16:42:44 -0500

    Completed project in 30 min

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { letters: "" };
+  }
+
+  onVowelCount = (event) => {
+    if (this.state.letters.length === 0) {
+      return;
+    }
+    event.preventDefault();
+    const allVowels = "aeiouy".split("");
+    let counter = 0;
+    this.state.letters.split("").forEach((letter) => {
+      if (allVowels.includes(letter)) {
+        counter++;
+      }
+    });
+
+    const hasVowels = `Number of vowels in your phrase: ${counter}`;
+    const noVowels = "There were no vowels in your phrase";
+    counter !== 0 ? alert(hasVowels) : alert(noVowels);
+    this.setState({ letters: "" });
+  };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row text-center">
+          <h1>Vowel Counter</h1>
+        </div>
+        <form>
+          <input
+            type="text"
+            id="myInput"
+            className="form-control"
+            aria-describedby="vowel-counter"
+            onChange={(e) => this.setState({ letters: e.target.value })}
+            value={this.state.letters}
+            required
+          />
+          <div className="row text-center">
+            <div className="col">
+              <button
+                type="submit"
+                className="btn btn-success mt-3"
+                onClick={this.onVowelCount}
+              >
+                Check
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
